9a34a15b62ff59e1b2f7f1ffdca00e68e16a39b8
kindred008 <talia.preston123@gmail.com>
2023-07-06 17:47:52 +0100

    Documented classes missing it

--- Runtime/Choice.cs
+++ Runtime/Choice.cs
@@ -12,11 +12,17 @@ namespace Kindred.Kindalogue.Runtime
 
         [SerializeField] private string m_nextDialogueId;
         
+        /// <summary>
+        /// Text that accompanies a choice so player knows what choice is. 
+        /// </summary>
         public string ChoiceText
         {
             get => m_choiceText;
         }
 
+        /// <summary>
+        /// ID of Dialogue from DialogueList to go to after making this choice. 
+        /// </summary>
         internal string NextDialogueId
         {
             get => m_nextDialogueId;
--- Runtime/Dialogue.cs
+++ Runtime/Dialogue.cs
@@ -19,29 +19,44 @@ namespace Kindred.Kindalogue.Runtime
 
         [SerializeField] private string m_nextDialogueId;
 
+        /// <summary>
+        /// UniqueID of this Dialogue. Allows Dialogue or Choices to link to this Dialogue object. This should be unique within a DialogueList.
+        /// </summary>
         public string UniqueId
         {
             get => m_uniqueId;
         }
 
+        /// <summary>
+        /// Who is performing this Dialogue. This will override default Actor set in DialogueList. Leave Null to not overrite.
+        /// </summary>
         public Actor Actor
         {
             get => m_actor;
             set => m_actor = value;
         }
 
+        /// <summary>
+        /// An array of strings that make up this dialogue line by line.
+        /// </summary>
         public string[] DialogueLines
         {
             get => m_dialogueLines;
             set => m_dialogueLines = value;
         }
 
+        /// <summary>
+        /// Array of choices that can be made.
+        /// </summary>
         public Choice[] Choices
         {
             get => m_choices;
             set => m_choices = value;
         }
 
+        /// <summary>
+        /// ID of Dialogue from DialogueList to go to next.
+        /// </summary>
         internal string NextDialogueId
         {
             get => m_nextDialogueId;
