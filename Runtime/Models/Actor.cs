dd4f75c917e531e98617227253d841427f3d1a2e
kindred008 <talia.preston123@gmail.com>
2024-05-08 18:40:48 +0100

    Created Actor scriptable object

---
+++ Runtime/Models/Actor.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "actor", menuName = "Kindalogue/Actor")]
+public class Actor: ScriptableObject
+{
+    [SerializeField] private string _uniqueId;
+    [SerializeField] private string _actorName;
+    [SerializeField] private Sprite _defaultSprite;
+    [SerializeField] private ActorSprite[] _sprites;
+
+    public string UniqueId
+    {
+        get => _uniqueId;
+    }
+
+    public string ActorName
+    {
+        get => _actorName;
+    }
+
+    public Sprite GetActorSprite()
+    {
+        return _defaultSprite;
+    }
+
+    public Sprite GetActorSprite(string spriteName)
+    {
+        var sprite = _sprites.SingleOrDefault(x => x.Name == spriteName).Sprite ?? _defaultSprite;
+
+        return sprite;
+    }
+
+    [Serializable]
+    private struct ActorSprite
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite _sprite;
+
+        public string Name
+        {
+            get => _name;
+        }
+
+        public Sprite Sprite
+        {
+            get => _sprite;
+        }
+    }
+}
